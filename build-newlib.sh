1bec6a4ac0206ec953a2b4d1c38c97b10e1c1266
willll <wilfried.fauvel@gmail.com>
2020-11-26 15:09:53 -0500

    Removed file extensions, it was breaking *NIX compatibility
    Removed file extensions, it was breaking *NIX compatibility

--- build-newlib.sh
+++ build-newlib.sh
@@ -7,17 +7,16 @@ mkdir -p $BUILDDIR/newlib
 cd $BUILDDIR/newlib
 
 export PATH=$INSTALLDIR/bin:$PATH
-export CROSS=${PROGRAM_PREFIX}elf-
-export CC_FOR_TARGET=${CROSS}gcc-${GCCVER}.exe
-export LD_FOR_TARGET=${CROSS}ld.exe
-export AS_FOR_TARGET=${CROSS}as.exe
-export AR_FOR_TARGET=${CROSS}ar.exe
-export RANLIB_FOR_TARGET=${CROSS}ranlib.exe
+export CROSS=${PROGRAM_PREFIX}
+export CC_FOR_TARGET=${CROSS}gcc
+export LD_FOR_TARGET=${CROSS}ld
+export AS_FOR_TARGET=${CROSS}as
+export AR_FOR_TARGET=${CROSS}ar
+export RANLIB_FOR_TARGET=${CROSS}ranlib
 
 export newlib_cflags="${newlib_cflags} -DPREFER_SIZE_OVER_SPEED -D__OPTIMIZE_SIZE__"
 
-#$SRCDIR/newlib-${NEWLIBVER}/configure --prefix=$INSTALLDIR \
-../../source/newlib-${NEWLIBVER}/configure --prefix=$INSTALLDIR \
+$SRCDIR/newlib-${NEWLIBVER}/configure --prefix=$INSTALLDIR \
 	--target=$TARGETMACH --build=$BUILDMACH --host=$HOSTMACH \
 	--enable-newlib-nano-malloc --enable-target-optspace \
 	--enable-lite-exit --disable-newlib-fvwrite-in-streamio \
