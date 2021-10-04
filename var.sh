4e9a73ea3c5d5b896cbf463877d1e24f88b8ecdb
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2021-10-04 18:54:09 -0400

    Update var.sh

--- var.sh
+++ var.sh
@@ -4,10 +4,24 @@ export RELSRCDIR=./toolchain/source
 export SRCDIR=$PWD/toolchain/source
 export BUILDDIR=$PWD/toolchain/build
 export TARGETMACH=sh-elf
-export BUILDMACH=mingw32
-#i686-pc-msys
-export HOSTMACH=mingw32
-#i686-pc-msys
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    export BUILDMACH=i686-pc-linux-gnu
+    export HOSTMACH=i686-pc-linux-gnu
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    export BUILDMACH=i686-pc-linux-gnu
+    export HOSTMACH=i686-pc-linux-gnu
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    export BUILDMACH=i686-pc-linux-gnu
+    export HOSTMACH=i686-pc-linux-gnu
+elif [[ "$OSTYPE" == "msys" ]]; then
+    export BUILDMACH=mingw32
+    export HOSTMACH=mingw32
+else
+    export BUILDMACH=i686-pc-linux-gnu
+    export HOSTMACH=i686-pc-linux-gnu
+fi
+
 export INSTALLDIR=$PWD/toolchain/toolchain
 export SYSROOTDIR=$INSTALLDIR/sysroot
 export ROOTDIR=$PWD/toolchain
@@ -29,10 +43,10 @@ export GMPREV=
 
 export OBJFORMAT=ELF
 
-#export TARGETMACH=sh-elf
-
 export BINUTILS_CFLAGS="-s"
 export GCC_BOOTSTRAP_FLAGS="--with-cpu=m2"
 export GCC_FINAL_FLAGS="--with-cpu=m2 --with-sysroot=$SYSROOTDIR"
 export NCPU=1
 export QTIFWDIR=./installer
+
+exec "$@"
