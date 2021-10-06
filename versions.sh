883eef899b7153b88693139761d44f58c84f7d0a
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2021-10-05 21:48:06 -0400

    Create versions.sh

---
+++ versions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export BINUTILSVER=2.34
+export BINUTILSREV=
+export GCCVER=8.4.0
+export GCCREV=
+export NEWLIBVER=3.3.0
+export NEWLIBREV=
+export MPCVER=1.1.0
+export MPCREV=
+export MPFRVER=4.0.2
+export MPFRREV=
+export GMPVER=6.2.0
+export GMPREV=
+
+exec "$@"
