4f3362fd1d95d0938b59735a24b23f8dfb946157
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2021-10-05 21:50:22 -0400

    Update var.sh

--- var.sh
+++ var.sh
@@ -28,19 +28,6 @@ export ROOTDIR=$PWD/toolchain
 export DOWNLOADDIR=$PWD/toolchain/download
 export PROGRAM_PREFIX=sh-
 
-export BINUTILSVER=2.34
-export BINUTILSREV=
-export GCCVER=8.4.0
-export GCCREV=
-export NEWLIBVER=3.3.0
-export NEWLIBREV=
-export MPCVER=1.1.0
-export MPCREV=
-export MPFRVER=4.0.2
-export MPFRREV=
-export GMPVER=6.2.0
-export GMPREV=
-
 export OBJFORMAT=ELF
 
 export BINUTILS_CFLAGS="-s"
@@ -49,4 +36,4 @@ export GCC_FINAL_FLAGS="--with-cpu=m2 --with-sysroot=$SYSROOTDIR"
 export NCPU=1
 export QTIFWDIR=./installer
 
-exec "$@"
+SOURCE versions.sh
