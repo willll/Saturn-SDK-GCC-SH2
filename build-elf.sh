dda3eda33a9d9ce76c91608e1f530fb52b31b090
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2021-10-06 21:19:24 -0400

    Update build-elf.sh

--- build-elf.sh
+++ build-elf.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-export OBJFORMAT=ELF
-
-export TARGETMACH=sh-elf
-
-if [ -z ${PROGRAM_PREFIX} ]; then
-	export PROGRAM_PREFIX=saturn-sh2-elf-
-else
-	export PROGRAM_PREFIX=${PROGRAM_PREFIX}elf-
-fi
-
-export BINUTILS_CFLAGS="-s"
-export GCC_BOOTSTRAP_FLAGS="--with-cpu=m2"
-export GCC_FINAL_FLAGS="--with-cpu=m2 --with-sysroot=$SYSROOTDIR"
-
 ./build.sh
 
 if [ $? -ne 0 ]; then
