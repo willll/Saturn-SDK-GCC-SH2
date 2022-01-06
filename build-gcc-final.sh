aad40198998c7921b20434ed7558f4831cd40212
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2022-01-05 20:50:35 -0500

    Update build-gcc-final.sh

--- build-gcc-final.sh
+++ build-gcc-final.sh
@@ -6,18 +6,11 @@ set -e
 mkdir $BUILDDIR/gcc-final
 cd $BUILDDIR/gcc-final
 
-#echo "libc_cv_forced_unwind=yes" > config.cache
-#echo "libc_ctors_header=yes" >> config.cache
-#echo "libc_cv_c_cleanup=yes" >> config.cache
-
 export PATH=$INSTALLDIR/bin:$PATH
 export CFLAGS="-s -DCOMMON_LVB_REVERSE_VIDEO=0x4000 -DCOMMON_LVB_UNDERSCORE=0x8000 -std=c99"
-export CXXFLAGS="-s -DCOMMON_LVB_REVERSE_VIDEO=0x4000 -DCOMMON_LVB_UNDERSCORE=0x8000 -std=c99"
+export CXXFLAGS="-s -DCOMMON_LVB_REVERSE_VIDEO=0x4000 -DCOMMON_LVB_UNDERSCORE=0x8000 -std=c++11"
 export CDIR=$PWD
 
-#cd ${SRCDIR}/gcc-${GCCVER}
-#`realpath --relative-to=./ ${SRCDIR}/gcc-${GCCVER}`/configure \
-#./configure \
 ../../source/gcc-${GCCVER}/configure \
 	--build=$BUILDMACH --target=$TARGETMACH --host=$HOSTMACH \
 	--prefix=$INSTALLDIR --enable-languages=c,c++,lto  --disable-bootstrap \
