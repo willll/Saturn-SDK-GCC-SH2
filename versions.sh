8d77c39e062826a845baa88eb34002d9ec8fce83
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2024-05-17 10:02:49 -0400

    GCC 14.1 WIP

--- versions.sh
+++ versions.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export BINUTILSVER=2.34
+export BINUTILSVER=2.42
 export BINUTILSREV=
-export GCCVER=8.4.0
+export GCCVER=14.1.0
 export GCCREV=
-export NEWLIBVER=3.3.0
+export NEWLIBVER=4.2.0
 export NEWLIBREV=
-export MPCVER=1.1.0
+export MPCVER=1.3.1
 export MPCREV=
-export MPFRVER=4.0.2
+export MPFRVER=4.2.1
 export MPFRREV=
-export GMPVER=6.2.0
+export GMPVER=6.3.0
 export GMPREV=
 
 exec "$@"
