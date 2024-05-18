58d3f03ea1cb195dcfa98a709fc9c67ad3c337e6
willll <wilfried.fauvel@gmail.com>
2024-05-18 17:19:37 -0400

    Removed LTO

--- build-gcc-final.sh
+++ build-gcc-final.sh
@@ -13,7 +13,7 @@ export CDIR=$PWD
 
 ../../source/gcc-${GCCVER}/configure \
 	--build=$BUILDMACH --target=$TARGETMACH --host=$HOSTMACH \
-	--prefix=$INSTALLDIR --enable-languages=c,c++,lto  --disable-bootstrap \
+	--prefix=$INSTALLDIR --enable-languages=c,c++  --disable-bootstrap \
 	--with-gnu-as --with-gnu-ld --disable-shared --disable-threads \
 	--disable-multilib --disable-libmudflap --enable-libssp --disable-lto \
 	--disable-install-libiberty \
