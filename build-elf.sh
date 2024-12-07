74d59392e5bb3887d3226b414fbcb9c91c0a85a7
nemesis-saturn <vasilkolarov@hotmail.com>
2024-12-08 00:52:18 +0300

    Added aarch64-aaple-darwin
    Added aarch64-aaple-darwin export variables for BUILDMACH and HOSTMACH

--- build-elf.sh
+++ build-elf.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Source environment variables from var-elf.sh
+source ./var-elf.sh
+
+# Run the build script
 ./build.sh
 
+# Check if the build script succeeded
 if [ $? -ne 0 ]; then
-	echo "Failed to build the ELF toolchain"
-	exit 1
+    echo "Failed to build the ELF toolchain"
+    exit 1
 fi
 
+# Create the installer if requested
 if [[ "${CREATEINSTALLER}" == "YES" ]]; then
-	./createinstaller.sh
+    ./createinstaller.sh
 fi
