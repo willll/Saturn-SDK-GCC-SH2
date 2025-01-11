c457bc6eb58823bb3f7b960d5394e8793e2b2aa5
willll <464311+willll@users.noreply.github.com>
2025-01-11 18:54:13 -0500

    gcc 12.4

--- versions.sh
+++ versions.sh
@@ -2,10 +2,10 @@
 
 export BINUTILSVER=2.41
 export BINUTILSREV=
-export GCCVER=13.3.0
+export GCCVER=12.4.0
 export GCCREV=
-export NEWLIBVER=4.2.0
-export NEWLIBREV=.20211231
+export NEWLIBVER=4.3.0
+export NEWLIBREV=
 export MPCVER=1.3.1
 export MPCREV=
 export MPFRVER=4.2.1
