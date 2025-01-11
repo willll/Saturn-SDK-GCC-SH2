31796deb832f20de703029e4e82d2b402fc2e953
willll <464311+willll@users.noreply.github.com>
2025-01-11 17:16:46 -0500

    Added ENABLE_STATIC_BUILD to statically build the toolchain

--- versions.sh
+++ versions.sh
@@ -13,4 +13,12 @@ export MPFRREV=
 export GMPVER=6.2.1
 export GMPREV=
 
+export GDBVER=
+#export GDBVER=14.2
+export GDBREV=
+
+export ENABLE_BOOTSTRAP=0
+export ENABLE_DOWNLOAD_CACHE=1
+export ENABLE_STATIC_BUILD=0
+
 exec "$@"
