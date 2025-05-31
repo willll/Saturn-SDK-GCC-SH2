2d5e94a4df7972dd6fe030b660de39cab2fe7c25
willll <464311+willll@users.noreply.github.com>
2025-05-31 18:57:37 -0400

    Update versions and add new binaries for binutils and gcc

--- versions.sh
+++ versions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export BINUTILSVER=2.43
+export BINUTILSVER=2.44
 export BINUTILSREV=
-export GCCVER=14.2.0
+export GCCVER=14.3.0
 export GCCREV=
 export NEWLIBVER=4.4.0
 export NEWLIBREV=.20231231
 export MPCVER=1.3.1
 export MPCREV=
-export MPFRVER=4.2.1
+export MPFRVER=4.2.2
 export MPFRREV=
 export GMPVER=6.3.0
 export GMPREV=
