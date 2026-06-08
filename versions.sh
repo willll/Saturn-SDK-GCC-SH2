2d9669740f5d2ebf1d14848b8fa088eb310d99dc
willll <464311+willll@users.noreply.github.com>
2026-06-08 18:33:31 -0400

    Remove GDB version assignment from versions.sh

--- versions.sh
+++ versions.sh
@@ -12,7 +12,8 @@ export MPFRVER=4.2.2
 export MPFRREV=
 export GMPVER=6.3.0
 export GMPREV=
-export GDBVER=14.2
+export GDBVER=
+#export GDBVER=14.2
 export GDBREV=
 
 export REQUIRED_AUTOMAKE_VERSION=1.17
